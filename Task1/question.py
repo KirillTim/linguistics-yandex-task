b64097fd0007177c4625cdd2acd76644f3d348e8
Kirill <timofeev.kirill.a@gmail.com>
2016-05-15 00:30:31 +0300

    github link add

--- Task1/question.py
+++ Task1/question.py
@@ -1,3 +1,5 @@
+# implementation details and more:
+# https://github.com/KirillTim/linguistics-yandex-task/tree/master/Task1
 from collections import namedtuple
 
 from nltk.stem.wordnet import WordNetLemmatizer
